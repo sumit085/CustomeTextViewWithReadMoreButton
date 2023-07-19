e47d344e85e6ee390a45eef3223d5be08f5987fa
Sumit <sumrathore0@gmail.com>
2023-07-19 08:25:39 +0530

    done

---
+++ CustomeTextViewWithReadMoreButton/CustomeTextViewWithReadMoreButton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomeTextViewWithReadMoreButton
+//
+//  Created by Admin on 01/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
